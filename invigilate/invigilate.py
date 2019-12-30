564dd4fede52efc6ec1558db9dfe9391f1c3b6d3
Erik Peterson <Erik.Exists@gmail.com>
2019-12-30 14:39:07 -0800

    Added replace flag

--- invigilate/invigilate.py
+++ invigilate/invigilate.py
@@ -20,12 +20,15 @@ class Store():
 
 class StoreBest():
     """Monitor variables, update conditionally."""
-    def __init__(self, monitor=None, intial_score=0):
+    def __init__(self, monitor=None, intial_score=0, replace=False):
         self.best_score = intial_score
+        self.replace = replace
         self.monitor = self.monitor
         self.data = {}
-        for k in self.monitor:
-            self.data[k] = list()
+
+        if not self.replace:
+            for k in self.monitor:
+                self.data[k] = list()
 
     def update(self, score, locals_dict):
         """Update the store, if the score has gotten better."""
@@ -33,4 +36,7 @@ class StoreBest():
             if score > self.best_score:
                 self.best_score = score
                 for k in self.data:
-                    self.data[k].append(locals_dict[k])
+                    if not self.replace:
+                        self.data[k].append(locals_dict[k])
+                    else:
+                        self.data[k] = locals_dict[k]
